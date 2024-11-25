be4e69a94531c40aede32f406ebe3b6cc8992566
NodoTecRuben <rrodriguez@sfvc.edu.ar>
2024-11-25 00:28:25 -0300

    add log and edit config

--- src/config/dbConfig.mjs
+++ src/config/dbConfig.mjs
@@ -4,12 +4,12 @@ export async function connectDB() {
     try {
         await mongoose.connect(
             'mongodb+srv://grupo-01:grupo01@cursadanodejs.ls9ii.mongodb.net/Node-js', {
-                serverSelectionTimeoutMS: 5000
-                // useNewUrlParser: true,
-                // useUnifiendTopology: true
-            }
+            serverSelectionTimeoutMS: 5000
+            // useNewUrlParser: true,
+            // useUnifiendTopology: true
+        }
         );
-        console.log('Conexión exitosa a MongoDB');
+        console.log('Conexión exitosa a MongoDB desde el servidor');
     } catch (error) {
         console.error('Error al conectar a MongoDB', error);
         procces.exit(1);
